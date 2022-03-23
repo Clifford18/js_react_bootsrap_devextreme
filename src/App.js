812ece7cd7d62e59560d00a8ab7078258afde357
Clifford18 <cliffordjohn.cj@gmail.com>
2022-03-23 16:11:42 +0300

    update  App.js file with request logs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
+import RequestLogsComponent from "./components/RequestLogsComponent";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div className="container">
+			<RequestLogsComponent/>
 		</div>
 	);
 }
