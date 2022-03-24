7ca739d31fbdc48a363b10f223de7c4d27d44a40
Clifford18 <cliffordjohn.cj@gmail.com>
2022-03-24 17:24:38 +0300

    update HeaderComponent.js file

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -4,6 +4,12 @@ class HeaderComponent extends Component {
 	render() {
 		return (
 			<div>
+				<header>
+					<nav className ="navbar nav-expand-md navbar-dark bg-dark">
+						<div><a className="navbar-brand">ATM Bridge Management App</a></div>
+					</nav>
+
+				</header>
 				
 			</div>
 		);
