011158453a41f4755156e53f3eef14075333a4cb
Clifford18 <cliffordjohn.cj@gmail.com>
2022-03-24 16:53:25 +0300

    update FooterComponent.js file

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -4,7 +4,9 @@ class FooterComponent extends Component {
 	render() {
 		return (
 			<div>
-				
+				<footer className = "footer">
+					<span className="text-muted">All Rights Reserved 2022 @Clifford18 </span>
+				</footer>
 			</div>
 		);
 	}
