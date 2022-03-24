8d1108e2acf262ca23521b292b0931796cd81b7b
Clifford18 <cliffordjohn.cj@gmail.com>
2022-03-24 17:25:52 +0300

    update App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import RequestLogsComponent from "./components/RequestLogsComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
 	return (
-		<div className="container">
-			<RequestLogsComponent/>
+		<div>
+			<HeaderComponent/>
+			<div className="container">
+				<RequestLogsComponent/>
+			</div>
+			<FooterComponent/>
+
 		</div>
+
 	);
 }
 
