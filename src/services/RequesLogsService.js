10e3c34c4b172cc262a1d1fd7b683b89ac8e3a8b
Clifford18 <cliffordjohn.cj@gmail.com>
2022-03-31 17:08:21 +0300

    update service

--- src/services/RequesLogsService.js
+++ src/services/RequesLogsService.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
-const REQUEST_API_BASE_URL = 'https://http://localhost:9081/api/rest/request-logs';
+const REQUEST_API_BASE_URL = 'http://localhost:9081/api/rest/request-logs';
 
-class RequesLogsService{
+class RequestLogsService{
 
 	 getRequestLogs(){
 return axios.get(REQUEST_API_BASE_URL);
 	 }
-}
 
-export default new RequesLogsService()
+	//console.log(getRequestLogs);
+
+}
+export default new RequestLogsService()
+
