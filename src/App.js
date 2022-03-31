d9b0726f6c1a2cc9d000079818b06224e530819c
Clifford18 <cliffordjohn.cj@gmail.com>
2022-03-31 17:07:41 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import RequestLogsComponent from "./components/RequestLogsComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
+import Task from "./components/tasks";
+import Request from "./components/Requests";
+import Test from "./components/Test";
+import Test1 from "./components/Test1";
+
 
 function App() {
 	return (
@@ -9,8 +14,13 @@ function App() {
 			<HeaderComponent/>
 			<div className="container">
 				<RequestLogsComponent/>
+				<Request/>
+				<Test/>
+				<Test1/>
+				<Task/>
 			</div>
 			<FooterComponent/>
+
 
 		</div>
 
