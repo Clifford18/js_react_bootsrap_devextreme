179dd0d7bc825273e141c29897b9b8701ed6d6fb
Clifford18 <cliffordjohn.cj@gmail.com>
2022-03-31 17:38:09 +0300

    update Test1 file

--- src/components/Test1.js
+++ src/components/Test1.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import 'devextreme/dist/css/dx.light.css';
 
-import DataGrid from 'devextreme-react/data-grid';
+import DataGrid, {Pager, Paging} from 'devextreme-react/data-grid';
 import SelectBox from 'devextreme-react/select-box';
 
 import service from './data1.js';
+import {Column, FilterRow} from "devextreme-react/gantt";
 
 export default class Test1 extends React.Component {
 	constructor(props) {
@@ -14,7 +15,7 @@ export default class Test1 extends React.Component {
 	}
 
 	getDisplayExpr(item) {
-		return item && item.FirstName + ' ' + item.request_method;
+		return item && item.request_method + ' ' + item.request_method;
 	}
 
 	render() {
@@ -24,10 +25,32 @@ export default class Test1 extends React.Component {
 				<DataGrid
 					className={'dx-card wide-card'}
 					dataSource={this.employees}
+					keyExpr="request_id"
+					showBorders={false}
 					focusedRowEnabled={true}
 					defaultFocusedRowIndex={0}
-					keyExpr="request_id"
+					columnAutoWidth={true}
+					columnHidingEnabled={true}
+				>
+				{/*<Paging defaultPageSize={10}/>*/}
+				{/*<Pager showPageSizeSelector={true} showInfo={true}/>*/}
+				{/*<FilterRow visible={true}/>*/}
+
+				<Column dataField={'request_id'} width={90} hidingPriority={2} />
+				<Column
+					dataField={'request_method'}
+					width={190}
+					caption={'request_method'}
+
 				/>
+				</DataGrid>
+
+
+
+
+
+
+
 			</React.Fragment>
 		);
 	}
