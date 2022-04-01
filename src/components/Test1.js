e6dc78da9d72df9245006508bc365beb56992adc
Clifford18 <cliffordjohn.cj@gmail.com>
2022-04-01 10:55:39 +0300

    apdate Test1.js file

--- src/components/Test1.js
+++ src/components/Test1.js
@@ -11,6 +11,94 @@ import {Column, FilterRow} from "devextreme-react/gantt";
 export default class Test1 extends React.Component {
 	constructor(props) {
 		super(props);
+		this.requests = service.getEmployees1();
+	}
+
+	getDisplayExpr(item) {
+		return item && item.request_method + ' ' + item.request_method;
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<h1 className={'content-block'}>Request Logs</h1>
+				<DataGrid
+					className={'dx-card wide-card'}
+					dataSource={this.requests}
+					showBorders={false}
+					focusedRowEnabled={true}
+					defaultFocusedRowIndex={0}
+					columnAutoWidth={true}
+					columnHidingEnabled={true}
+				>
+					<Paging defaultPageSize={10}/>
+					<Pager showPageSizeSelector={true} showInfo={true}/>
+					<FilterRow visible={true}/>
+
+					<Column dataField={'request_id'} width={90} hidingPriority={2}/>
+					<Column
+						dataField={'request_reference'}
+						width={150}
+						caption={'Request Reference'}
+					/>
+					<Column
+						dataField={'request_method'}
+						width={140}
+						caption={'Request Method'}
+					/>
+					<Column
+						dataField={'request_headers'}
+						width={140}
+						allowSorting={false}
+						caption={'Request Headers'}
+					/>
+					<Column
+						dataField={'request_body'}
+						width={190}
+						allowSorting={false}
+						caption={'Request Body'}
+					/>
+					<Column
+						dataField={'response_headers'}
+						width={190}
+						allowSorting={false}
+						caption={'Response Headers'}
+					/>
+					<Column
+						dataField={'response_body'}
+						width={190}
+						allowSorting={false}
+						caption={'Response Body'}
+					/>
+					<Column
+						dataField={'error_code'}
+						width={100}
+						caption={'Error Code'}
+					/>
+					<Column
+						dataField={'date_created'}
+						width={190}
+						caption={'Date Created'}
+						dataType={'date'}
+
+					/>
+					<Column
+						dataField={'date_modified'}
+						width={190}
+						caption={'Date Modified'}
+						dataType={'date'}
+
+					/>
+				</DataGrid>
+
+			</React.Fragment>
+		);
+	}
+}
+
+export class Test2 extends React.Component {
+	constructor(props) {
+		super(props);
 		this.employees = service.getEmployees1();
 	}
 
@@ -32,24 +120,8 @@ export default class Test1 extends React.Component {
 					columnAutoWidth={true}
 					columnHidingEnabled={true}
 				>
-				{/*<Paging defaultPageSize={10}/>*/}
-				{/*<Pager showPageSizeSelector={true} showInfo={true}/>*/}
-				{/*<FilterRow visible={true}/>*/}
 
-				<Column dataField={'request_id'} width={90} hidingPriority={2} />
-				<Column
-					dataField={'request_method'}
-					width={190}
-					caption={'request_method'}
-
-				/>
 				</DataGrid>
-
-
-
-
-
-
 
 			</React.Fragment>
 		);
