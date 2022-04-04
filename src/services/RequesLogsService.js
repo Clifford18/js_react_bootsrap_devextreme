c2f11b4db87965b4ac2127f12cd92a773c054072
Clifford18 <cliffordjohn.cj@gmail.com>
2022-04-04 11:34:20 +0300

    update RequesLogsService.js file

--- src/services/RequesLogsService.js
+++ src/services/RequesLogsService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const REQUEST_API_BASE_URL = 'http://localhost:9081/api/rest/request-logs';
+const REQUEST_API_BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 class RequestLogsService{
 
