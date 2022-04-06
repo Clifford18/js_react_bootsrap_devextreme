cdc8c1b057fe1e008aa94c1581d503bf2e7086cc
Clifford18 <cliffordjohn.cj@gmail.com>
2022-04-06 17:02:47 +0300

    add and update NavDataTest.js file

---
+++ src/NavTest/NavDataTest.js
@@ -0,0 +1,116 @@
+export const REQUESTLOGSTYPES = [{
+	id: '1',
+	text: 'Request Logs',
+	expanded: true,
+	items: [{
+		id: '1_1',
+		text: 'ALL LOGS',
+		fullName: 'Arab Republic of Egypt',
+		description: 'Egypt is a transcontinental country spanning the northeast corner of Africa and southwest corner of Asia by a land bridge the Sinai Peninsula forms.',
+		area: '1,010,407.87',
+		population: '94,798,827',
+		gdp: '1,173',
+		selected: true,
+		flag: 'images/flags/Egypt.svg',
+		cities: [{
+			id: '1_1_1',
+			text: 'Cairo',
+			population: '9,500,000',
+			area: '528',
+			density: '19,376',
+			description: "The city's metropolitan area is the largest in the Middle East and the Arab world, and the 15th-largest in the world, is associated with ancient Egypt.",
+			capital: true,
+			flag: 'images/flags/Cairo.svg'
+		}, {
+			id: '1_1_2',
+			text: 'Alexandria',
+			population: '4,984,387',
+			area: '2,679',
+			density: '1,900',
+			description: 'Alexandria is the second largest city and a major economic center in Egypt, extending about 32 km (20 mi) along the coast of the Mediterranean Sea in the north central part of the country.',
+			flag: 'images/flags/Alexandria.svg'
+		}, {
+			id: '1_1_3',
+			text: 'Giza',
+			population: '3,628,062',
+			area: '1,579.75',
+			density: '2,300',
+			description: 'Giza is the third-largest city in Egypt. It is located on the west bank of the Nile, 5 km (3 mi) southwest of central Cairo.',
+			flag: 'images/flags/Giza.svg'
+		}]
+	},
+		{
+			id: '1_2',
+			text: 'ATM BRIDGE',
+			fullName: 'Republic of South Africa',
+			description: 'South Africa is the southernmost country in Africa. It is bounded on the south by 2,798 kilometres (1,739 mi) of coastline of Southern Africa stretching along the South Atlantic and Indian Oceans.',
+			area: '1,221,037',
+			population: '54,956,900',
+			gdp: '742.461',
+			flag: 'images/flags/SouthAfrica.svg',
+			cities: [{
+				id: '1_2_2',
+				text: 'Pretoria',
+				population: '741,651',
+				area: '687.54',
+				density: '1,100',
+				description: "Pretoria is a city in the northern part of Gauteng, South Africa. Being one of the country's three capital cities, it serves as the seat of the executive branch of government.",
+				capital: true,
+				flag: 'images/flags/Pretoria.svg'
+			}, {
+				id: '1_2_1',
+				text: 'Johannesburg',
+				population: '957,441',
+				area: '334.81',
+				density: '2,900',
+				description: 'Johannesburg is the largest city in South Africa and is one of the 50 largest urban areas in the world.',
+				flag: 'images/flags/Johannesburg.svg'
+			}, {
+				id: '1_2_3',
+				text: 'Durban',
+				population: '595,061',
+				area: '225.91',
+				density: '2,600',
+				description: 'Durban is the largest city in the South African province of KwaZulu-Natal. It is Located on the Indian Ocean coast of the African continent.',
+				flag: 'images/flags/Durban.svg'
+			}]
+		},
+		{
+			id: '1_3',
+			text: 'Members Portal',
+			fullName: 'Arab Republic of Egypt',
+			description: 'Egypt is a transcontinental country spanning the northeast corner of Africa and southwest corner of Asia by a land bridge the Sinai Peninsula forms.',
+			area: '1,010,407.87',
+			population: '94,798,827',
+			gdp: '1,173',
+			selected: true,
+			flag: 'images/flags/Egypt.svg',
+			cities: [{
+				id: '1_3_1',
+				text: 'Cairo',
+				population: '9,500,000',
+				area: '528',
+				density: '19,376',
+				description: "The city's metropolitan area is the largest in the Middle East and the Arab world, and the 15th-largest in the world, is associated with ancient Egypt.",
+				capital: true,
+				flag: 'images/flags/Cairo.svg'
+			}, {
+				id: '1_3_2',
+				text: 'Alexandria',
+				population: '4,984,387',
+				area: '2,679',
+				density: '1,900',
+				description: 'Alexandria is the second largest city and a major economic center in Egypt, extending about 32 km (20 mi) along the coast of the Mediterranean Sea in the north central part of the country.',
+				flag: 'images/flags/Alexandria.svg'
+			}, {
+				id: '1_3_3',
+				text: 'Giza',
+				population: '3,628,062',
+				area: '1,579.75',
+				density: '2,300',
+				description: 'Giza is the third-largest city in Egypt. It is located on the west bank of the Nile, 5 km (3 mi) southwest of central Cairo.',
+				flag: 'images/flags/Giza.svg'
+			}]
+		}
+	]
+}];
