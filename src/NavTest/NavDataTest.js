c6583825bcae56bf87de84221a04d2de1481a538
Clifford18 <cliffordjohn.cj@gmail.com>
2022-04-07 16:04:35 +0300

    update NavDataTest.js on JSON

--- src/NavTest/NavDataTest.js
+++ src/NavTest/NavDataTest.js
@@ -8,7 +8,7 @@ export const REQUESTLOGSTYPES = [{
 		fullName: 'All Enterprise Service Bus Logs',
 		description: 'This is a combination of request log made both to the ATM Bridge and Members portal and their corresponding response',
 		selected: true,
-		cities: [{
+		options: [{
 			id: '1_1_1',
 			text: 'Cairo'
 		}]
@@ -18,7 +18,7 @@ export const REQUESTLOGSTYPES = [{
 			text: 'ATM Bridge Logs',
 			fullName: 'ATM Bridge Logs',
 			description: 'These are request logs made only to the ATM Bridge and their corresponding response',
-			cities: [{
+			options: [{
 				id: '1_2_2',
 				text: 'Pretoria'
 			}]
@@ -28,9 +28,10 @@ export const REQUESTLOGSTYPES = [{
 			text: 'Members Portal Logs',
 			fullName: 'Members Portal Request Logs',
 			description: 'These are request logs made only to the Members and their corresponding response',
-			cities: [{
+			options: [{
 				id: '1_3_1',
-				text: 'Kuala Lumpur'
+				text: 'Kuala Lumpur',
+				description: 'Kuala Lumpur, officially the Federal Territory of Kuala Lumpur, or commonly KL, is the largest city of Malaysia. It is located in Klang Valley.'
 			}]
 		}
 	]
@@ -41,9 +42,9 @@ export const REQUESTLOGSTYPES = [{
 		items: [{
 			id: '2_1',
 			text: 'My Profile',
-			fullName: 'Japan',
-			description: 'Japan is a sovereign island nation in East Asia. Located in the Pacific Ocean, it lies off the eastern coast of the Asian mainland and stretches from the Sea of Okhotsk in the north to the East China Sea and China in the southwest.',
-			cities: [{
+			fullName: 'Profile',
+			description: 'My Profile',
+			options: [{
 				id: '2_1_1',
 				text: 'About Me '
 			}, {
