23e6df5ecb970f06d4e0219171194ae0d8401e28
Clifford18 <cliffordjohn.cj@gmail.com>
2022-06-03 14:38:20 +0300

    update NavDataTest.js file json

--- src/NavTest/NavDataTest.js
+++ src/NavTest/NavDataTest.js
@@ -4,9 +4,9 @@ export const REQUESTLOGSTYPES = [{
 	expanded: true,
 	items: [{
 		id: '1_1',
-		text: 'ALL LOGS',
-		title: 'All Enterprise Service Bus Logs',
-		description: 'This is a combination of request log made both to the ATM Bridge and Members portal and their corresponding response',
+		text: 'ATM Bridge Logs',
+		title: 'ATM Bridge Logs',
+		description: 'These are request logs made only to the ATM Bridge and their corresponding response',
 		selected: true,
 		options: [{
 			id: '1_1_1',
@@ -15,23 +15,12 @@ export const REQUESTLOGSTYPES = [{
 	},
 		{
 			id: '1_2',
-			text: 'ATM Bridge Logs',
-			title: 'ATM Bridge Logs',
-			description: 'These are request logs made only to the ATM Bridge and their corresponding response',
-			options: [{
-				id: '1_2_2',
-				text: 'ATM Bridge Log'
-			}]
-		},
-		{
-			id: '1_3',
 			text: 'Members Portal Logs',
 			title: 'Members Portal Request Logs',
 			description: 'These are request logs made only to the Members and their corresponding response',
 			options: [{
-				id: '1_3_1',
-				text: 'Members Portal Request Logs',
-				description: 'Kuala Lumpur, officially the Federal Territory of Kuala Lumpur, or commonly KL, is the largest city of Malaysia. It is located in Klang Valley.'
+				id: '1_2_2',
+				text: 'ATM Bridge Log'
 			}]
 		}
 	]
@@ -51,6 +40,20 @@ export const REQUESTLOGSTYPES = [{
 				id: '2_1_2',
 				text: 'Reset Password'
 			}]
-		}]
+			
+		},
+			{
+				id: '2_2',
+				text: 'Reset Password',
+				title: 'Reset Password',
+				description: 'My Profile',
+				options: [{
+					id: '2_2_1',
+					text: 'About Me '
+				}, {
+					id: '2_2_2',
+					text: 'Reset Password'
+				}]
+			}]
 	}
 ];
